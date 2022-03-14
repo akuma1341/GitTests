39755ddf9158a9defedd434c0e25b31adeed0960
akuma1341 <akuma1341@gmail.com>
2022-03-14 15:32:15 +0300

    Initial commit

---
+++ src/main/java/com/example/gittests/GitTestsApplication.java
@@ -0,0 +1,13 @@
+package com.example.gittests;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitTestsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitTestsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/gittests/GitTestsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gittests;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitTestsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
